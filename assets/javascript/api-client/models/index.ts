bce56cdf00de5fef4afd3d22581a3bd61cbcf426
Rob <>
2023-01-16 21:47:24 -0500

    saas pegasus unedited

---
+++ assets/javascript/api-client/models/index.ts
@@ -0,0 +1,23 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AggregateEmployeeData';
+export * from './CreateCheckout';
+export * from './DepartmentEnum';
+export * from './Employee';
+export * from './Invitation';
+export * from './Membership';
+export * from './PaginatedEmployeeList';
+export * from './PaginatedInvitationList';
+export * from './PaginatedTeamList';
+export * from './PatchedEmployee';
+export * from './PatchedInvitation';
+export * from './PatchedTeam';
+export * from './Price';
+export * from './Product';
+export * from './ProductMetadata';
+export * from './ProductWithMetadata';
+export * from './RoleEnum';
+export * from './StatusEnum';
+export * from './Subscription';
+export * from './SubscriptionItem';
+export * from './Team';
---
+++ bmt_gpt/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for bmt-gpt-saas project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bmt_gpt.settings')
+
+application = get_wsgi_application()
