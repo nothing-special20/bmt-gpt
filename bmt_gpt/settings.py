772917432fe53beff3cd6fbb031a2ff22d060023
Rob <>
2023-02-24 17:15:24 -0500

    update STATIC_URL value

--- bmt_gpt/settings.py
+++ bmt_gpt/settings.py
@@ -258,7 +258,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_ROOT = BASE_DIR / 'static_root'
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
